afe157b80aff7303866696886030d77f6144e503
eyadcode24 <eyadcode24@gmail.com>
2024-05-26 23:56:33 +0200

    uuu

--- app.js
+++ app.js
@@ -1,32 +1,35 @@
 import PromptSync from "prompt-sync";
 
-const ingredients = ["harina", "sal", "aguaTibia", "aceite", " LevaduraFresca " ]
-const toping = ["salsa  de tomate", "mazurela", "atun" ]
-const pizzaType = [{name: "peperoni", ingredientsTopping: ["tomate", "mazorella", "atun"]}, {name: "margarita", ingredientsTopping: ["tomate", "mazorella"]}]
+const ingredients = ["harina", "sal", "aguaTibia", "aceite", " LevaduraFresca " ];
+const toping = ["salsa  de tomate", "mazurela", "atun" ];
+const pizzaType = [{name: "peperoni", ingredientsTopping: ["tomate", "mazorella", "atun"]}, {name: "margarita", ingredientsTopping: ["tomate", "mazorella"]}];
 let pizza = ""
 
-import PromptSync from "prompt-sync";
+
 
 function app ( ){
     const prompt = PromptSync();
-    const pizzaName = prompt("elige tu pizzaType");
-    
+    const pizzaName = prompt("elige tu pizzaType: ");
+    let result = makePizza(pizzaName);
+}
+function  makePizza(){
+
     function mixingridiente(){
     console.log( "add harina " + ingredients[0] + "add levadura  mas " + ingredients[1] + "add agua" + ingredients[2] + "add sal" + ingredients[3] + "add aceite " + ingredients[4])
 }
     function knead(){
-    console.log("mix all ingredients togather")
+    console.log("mix all ingredients togather");
 }
     function pizzaRest(){
     console.log("dejar la masa reposar durante 90 min");
 }
     function toppigAdd(){
-    console.log("add topping items" + toping[0] + toping[1] + toping[2])
+    console.log("add topping items" + toping[0] + toping[1] + toping[2]);
 }
     function hornear(){
-    console.log("hornear en temperatuea 250 durante 20min")
+    console.log("hornear en temperatuea 250 durante 20min");
 }
 }
 
 
-app()
+app();
