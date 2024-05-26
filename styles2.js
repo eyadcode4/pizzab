b1b727d6b05b495fc7a5b6bc4e3842cb6422ce43
eyadcode24 <eyadcode24@gmail.com>
2024-05-26 23:21:33 +0200

    fourth

--- styles2.js
+++ styles2.js
@@ -5,23 +5,29 @@ const toping = ["salsa  de tomate", "mazurela", "atun" ]
 const pizzaType = [{name: "peperoni", ingredientsTopping: ["tomate", "mazorella", "atun"]}, {name: "margarita", ingredientsTopping: ["tomate", "mazorella"]}]
 let pizza = ""
 
-function app ( ){
-    function mixingridiente(){
+function mixingridiente(){
     console.log( "add harina " + integradient[0] + "add levadura  mas " + integradient[1] + "add agua" + integradient[2] + "add sal" + integradient[3] + "add aceite " + integradient[4])
 }
-    function knead(){
+function knead(){
     console.log("mix all ingredients togather")
 }
-    function pizzaRest(){
+function pizzaRest()
+{
     console.log("dejar la masa reposar durante 90 min");
 }
-    function toppigAdd(){
-    console.log("add topping items" + toping[0] + toping[1] + toping[2])
+function toppigAdd(){
+    console.log("add topping items")
 }
-    function hornear(){
+function hornear(){
     console.log("hornear en temperatuea 250 durante 20min")
 }
+function app() {
+    const prompt = PromptSync();
+    const pizzaName = prompt(" what Are you need ");
+    let result = makePizza(pizzaName);
+    result.then((resolve) => {
+    if (resolve === undefined) deliverPizza();
+});
 }
-
-
 app()
+console.log("dejar la masa reposar durante 90 min");
